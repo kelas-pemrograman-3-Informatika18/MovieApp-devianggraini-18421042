4fca2daa048f8281d27d7dc5ddee73421ce811e4
unknown <devi.18421042@student.ubl.ac.id>
2020-07-05 13:42:11 +0700

    first commit

---
+++ src/pages/user/About.vue
@@ -0,0 +1,47 @@
+<template>
+  <q-layout class="bg-indigo-1" view="lHh Lpr Lff">
+    <q-page-container>
+      <q-page padding class="row items-center justify-center">
+        <div class="row full-width">
+          <div class="col-md-8 offset-md-2 col-xs-12 q-pa-md">
+            <q-card flat class="text-blue-grey-14">
+              <div class="row items-center">
+                <div class="col-md-12 col-sm-12-col-xs-12">
+                  <div class="row q-pt-md q-pb-md">
+                    <div class="col-md-8 offset-2">
+                      <q-img src="statics/about.png"></q-img>
+                    <div class="text-h5">About Movie App</div>
+                    <q-card-section>
+                      Movie App adalah aplikasi penjualan DVD Film berbasis web yang dibuat oleh Devi Anggraini.
+                      Aplikasi ini dibuat untuk mempermudah masyarakat dalam memesan dvd film secara online.
+                    </q-card-section>
+                    <div class="q-mt-md">
+                    <div class="text-h6">Contact Person :</div>
+                    <q-item>
+                     <q-item-section avatar>
+                      <q-icon name="phone"/>
+                    </q-item-section>
+                    <q-item-section>
+                      <q-item-label>0895352739848</q-item-label>
+                    </q-item-section>
+                    </q-item>
+                    <q-item>
+                     <q-item-section avatar>
+                      <q-icon name="mail"/>
+                    </q-item-section>
+                    <q-item-section>
+                      <q-item-label>movieapp@gmail.com</q-item-label>
+                    </q-item-section>
+                    </q-item>
+                    </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </q-card>
+          </div>
+        </div>
+      </q-page>
+    </q-page-container>
+  </q-layout>
+</template>
